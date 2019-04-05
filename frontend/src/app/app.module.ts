7365e01b59cc64dbc7be88608e115ef54d354db0
Camila Buitrago <macamy95@gmail.com>
2019-04-04 21:53:38 -0500

    Arreglo archivo app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,7 +8,11 @@ import { FooterComponent } from './components/footer/footer.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { EncabezadoComponent } from './components/encabezado/encabezado.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EstacionCrearComponent } from './components/estacion/estacion-crear/estacion-crear.component';
+import { EstacionConsultarComponent } from './components/estacion/estacion-consultar/estacion-consultar.component';
 import { MaterialModule } from 'src/material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
 import { TroncalComponent } from './components/troncal/troncal.component';
 import { TroncalCrearComponent } from './components/troncal/troncal-crear/troncal-crear.component';
 import { TroncalConsultarComponent } from './components/troncal/troncal-consultar/troncal-consultar.component';
@@ -18,16 +22,7 @@ import { RutaConsultarComponent } from './components/ruta/ruta-consultar/ruta-co
 import { GenerarDatosComponent } from './components/cargar-archivo/generar-datos/generar-datos.component';
 import { ServicioWebComponent } from './components/cargar-archivo/servicio-web/servicio-web.component';
 import { ReportesComponent } from './components/reportes/reportes.component';
-
 import { CargarArchivoComponent } from './components/cargar-archivo/cargar-archivo.component';
-import { EstacionCrearComponent } from './components/estacion/estacion-crear/estacion-crear.component';
-import { EstacionConsultarComponent } from './components/estacion/estacion-consultar/estacion-consultar.component';
-<<<<<<< HEAD
-=======
-import { MaterialModule } from 'src/material.module';
-import { HttpClientModule } from '@angular/common/http';
-import { ToastrModule } from 'ngx-toastr';
->>>>>>> development
 
 @NgModule({
   declarations: [
@@ -37,7 +32,7 @@ import { ToastrModule } from 'ngx-toastr';
     InicioComponent,
     EncabezadoComponent,
     EstacionCrearComponent,
-    EstacionConsultarComponent,
+    EstacionConsultarComponent
     TroncalComponent,
     TroncalCrearComponent,
     TroncalConsultarComponent,
